ac4fe7c91412ec18397d03680d8fbef26896eda1
Mani Gurusankar <mgurusa@us.ibm.com>
2016-04-20 13:33:55 -0400

    Add basic bootstrap layout

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+
 	var photoArr = [];
 
 	var flickr = new Flickr({
@@ -6,8 +7,8 @@ $( document ).ready(function() {
 	});
 
 	flickr.people.getPhotos({
-		api_key: "a5e95177da353f58113fd60296e1d250",
-		user_id: "24662369@N07"
+		user_id: "24662369@N07",
+		extras: "description"
 	}, function(err, result) {
 		var allPhotos = result.photos.photo;
 
@@ -17,15 +18,20 @@ $( document ).ready(function() {
 				"farm": photo.farm,
 				"id": photo.id,
 				"server": photo.server,
-				"secret": photo.secret
+				"secret": photo.secret,
+				"urlDefault": 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg',
+				"description": photo.description._content,
+				"title": photo.title
 			});
 		});
 
 		//get each photo and append to the html page
-		photoArr.forEach(function(photo){
-			var urlDefault = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
-			$(".photoGrid").append("<img src="+ urlDefault +">");
-		})
-
+		photoArr.forEach(function(photo, index){
+			if(index%3 ===0){
+				$(".photoGrid").append("<div class='row'><div class='col-md-4'><h3>"+photo.title +"</h3><a href='#' class='thumbnail'><img src="+ photo.urlDefault +"></a><p>"/*+ photo.description*/ +"</p></div></div>");
+			} else {
+				$(".photoGrid .row:last").append("<div class='col-md-4'><h3>"+photo.title +"</h3><a href='#' class='thumbnail'><img src="+ photo.urlDefault +"></a><p>"/*+ photo.description */+"</p></div>");
+			}
+		});
 	});
 });
